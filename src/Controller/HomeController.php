5924818cc289a23d44494a03a1cee580a86b56d9
ImPADfr <peter1_49@hotmail.com>
2024-12-13 11:20:29 +0100

    debugg et affichage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
+use App\Entity\Livres;
+use App\Entity\User;
+use App\Repository\CommentaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CommentaireRepository $commentaireRepository): Response
     {
+        $commentaires = $commentaireRepository->findBy([], ['createdAt' => 'DESC'], 3);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'commentaires' => $commentaires
         ]);
     }
 }
